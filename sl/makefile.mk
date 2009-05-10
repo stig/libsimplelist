5cb53326454d0ff143c79932273cbc8493b6fbba
sbrautaset <sbrautaset@d0e5e53a-1f52-11de-be07-9135ae5da72a>
2009-05-10 22:14:34 +0000

    r2089@Toko (orig r2088):  stig | 2006-12-21 11:52:49 +0000   r2556@Figleaf:  stig | 2006-12-21 11:58:32 +0100   changelog and bumped version numbers

--- sl/makefile.mk
+++ sl/makefile.mk
@@ -2,8 +2,8 @@ lib_LTLIBRARIES		= libsl.la
 
 nobase_include_HEADERS	= sl/sl.h
 libsl_la_SOURCES	= sl/sl.c
-libsl_la_LDFLAGS	= -no-undefined -version-info 1:0:1
+libsl_la_LDFLAGS	= -no-undefined -version-info 1:1:1
 
-man_MANS		      = sl/sl.3
+man_MANS		= sl/sl.3
 
 
