2f500446cfb837bbf8d1ab3c7c62c17948901960
sbrautaset <sbrautaset@d0e5e53a-1f52-11de-be07-9135ae5da72a>
2009-05-10 22:13:27 +0000

    r1821@Toko (orig r1820):  stig | 2006-04-15 20:04:30 +0100  update copyright notices & copy of license

--- sl/sl.h
+++ sl/sl.h
@@ -1,13 +1,24 @@
 /*
- * sl - a small and efficient linked list library
- *
- * Copyright (C) 2003,2004,2005 Stig Brautaset
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- */
+SL - A small and efficient linked list library.
+Copyright (C) 2003-2005 Stig Brautaset. All rights reserved.
+
+This file is part of SL.
+
+SL is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+SL is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SL; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
 
 #ifndef sl__sl_h
 #define sl__sl_h
